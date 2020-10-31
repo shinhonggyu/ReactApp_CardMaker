3aa6160e8648ad7f4b4246f32a60273969458423
honggyu <zowoz0@naver.com>
2020-10-31 18:40:46 +0900

    handle state change on editor

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Header from '../header/header'
-import Footer from '../footer/footer'
-import styles from './maker.module.css'
-import { useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
-import Editor from '../editor/editor'
-import Preview from '../preview/preview'
-import { useState } from 'react'
+import React from 'react';
+import Header from '../header/header';
+import Footer from '../footer/footer';
+import styles from './maker.module.css';
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import Editor from '../editor/editor';
+import Preview from '../preview/preview';
+import { useState } from 'react';
 
 const Maker = ({ authService }) => {
-  const [cards, setCards] = useState([
-    {
+  const [cards, setCards] = useState({
+    1: {
       id: '1',
       name: 'Ellie1',
       company: 'Samsung',
@@ -21,7 +21,7 @@ const Maker = ({ authService }) => {
       fileName: 'ellie',
       fileURL: null,
     },
-    {
+    2: {
       id: '2',
       name: 'Ellie2',
       company: 'Samsung',
@@ -32,7 +32,7 @@ const Maker = ({ authService }) => {
       fileName: 'ellie',
       fileURL: 'ellie.png',
     },
-    {
+    3: {
       id: '3',
       name: 'Ellie3',
       company: 'Samsung',
@@ -43,36 +43,53 @@ const Maker = ({ authService }) => {
       fileName: 'ellie',
       fileURL: null,
     },
-  ])
-  const history = useHistory()
+  });
+
+  const history = useHistory();
 
   const onLogout = () => {
-    authService.logout()
-  }
+    authService.logout();
+  };
 
   useEffect(() => {
     authService.onAuthChange((user) => {
       if (!user) {
-        history.push('/')
+        history.push('/');
       }
-    })
-  })
+    });
+  });
 
-  const addCard = (card) => {
-    const updated = [...cards, card]
-    setCards(updated)
-  }
+  const createOrUpdateCard = (card) => {
+    setCards((cards) => {
+      const updated = { ...cards };
+      updated[card.id] = card;
+      return updated;
+    });
+  };
+
+  const deleteCard = (card) => {
+    setCards((cards) => {
+      const updated = { ...cards };
+      delete updated[card.id];
+      return updated;
+    });
+  };
 
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor cards={cards} addCard={addCard} />
+        <Editor
+          cards={cards}
+          addCard={createOrUpdateCard}
+          updateCard={createOrUpdateCard}
+          deleteCard={deleteCard}
+        />
         <Preview cards={cards} />
       </div>
       <Footer />
     </section>
-  )
-}
+  );
+};
 
-export default Maker
+export default Maker;
